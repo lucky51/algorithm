17fdc68199db5ae69afd101333a2412893df2adf
lucky51 <gopher666@163.com>
2022-03-29 16:44:13 +0800

    add comment

--- backtracking/generate_parenthesis.go
+++ backtracking/generate_parenthesis.go
@@ -2,6 +2,8 @@ package backtracking
 
 var gpRes []string
 
+// generateParenthesis Leetcode 22 括号生成
+// 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
 func generateParenthesis(n int) []string {
 	gpRes = make([]string, 0)
 	if n == 0 {
