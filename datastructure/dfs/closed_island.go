ca15af2e50c7251fa0f6dfbf3ad740dedcb7d247
lucky51 <gopher666@163.com>
2022-04-08 09:20:00 +0800

    简化封闭岛屿问题的代码

--- datastructure/dfs/closed_island.go
+++ datastructure/dfs/closed_island.go
@@ -1,6 +1,6 @@
 package dfs
 
-// leetcode 1254. 统计封闭岛屿的数目 TODO: leetcode 还未提交
+// leetcode 1254. 统计封闭岛屿的数目
 //二维矩阵 grid由 0（土地）和 1（水）组成。岛是由最大的4个方向连通的 0组成的群，封闭岛是一个完全 由1包围（左、上、右、下）的岛。
 //请返回 封闭岛屿 的数目
 func closedIsland(grid [][]int) int {
@@ -18,37 +18,43 @@ func closedIsland(grid [][]int) int {
 }
 
 func dfs(grid [][]int, r, c int, result bool) bool {
-	grid[r][c] = 2
+	//if isBorder(grid, r, c) {
+	//	return false
+	//}
+	if r == 0 || c == 0 || r == len(grid)-1 || c == len(grid[r])-1 {
+		return false
+	}
+	grid[r][c] = 1
 	if r-1 >= 0 && grid[r-1][c] == 0 {
-		if isBorder(grid, r-1, c) {
-			result = false
-		} else {
-			// result放到后边，避免提前短路
-			result = dfs(grid, r-1, c, result) && result
-		}
+		//if isBorder(grid, r-1, c) {
+		//	result = false
+		//} else {
+		// result放到后边，避免提前短路
+		result = dfs(grid, r-1, c, result) && result
+		//}
 	}
 	if r+1 < len(grid) && grid[r+1][c] == 0 {
-		if isBorder(grid, r+1, c) {
-			result = false
-		} else {
-			result = dfs(grid, r+1, c, result) && result
-		}
+		//if isBorder(grid, r+1, c) {
+		//	result = false
+		//} else {
+		result = dfs(grid, r+1, c, result) && result
+		//}
 
 	}
 	if c-1 >= 0 && grid[r][c-1] == 0 {
-		if isBorder(grid, r, c-1) {
-			result = false
-		} else {
-			result = dfs(grid, r, c-1, result) && result
-		}
+		//if isBorder(grid, r, c-1) {
+		//	result = false
+		//} else {
+		result = dfs(grid, r, c-1, result) && result
+		//}
 
 	}
 	if c+1 < len(grid[r]) && grid[r][c+1] == 0 {
-		if isBorder(grid, r, c+1) {
-			result = false
-		} else {
-			result = dfs(grid, r, c+1, result) && result
-		}
+		//if isBorder(grid, r, c+1) {
+		//	result = false
+		//} else {
+		result = dfs(grid, r, c+1, result) && result
+		//}
 	}
 	return result
 }
